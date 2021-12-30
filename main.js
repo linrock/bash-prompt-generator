b37bfdf1d63aaf3a3fbc880a04e13bcc87013211
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-30 10:22:29 -0500

    More placeholders, add initial prompt colors

---
+++ main.js
@@ -0,0 +1,16 @@
+{
+  const username = document.querySelector(".username");
+  username.style.color = '#ffff00';
+
+  const separator = document.querySelector(".separator");
+  separator.style.color = '#ffd700';
+
+  const hostname = document.querySelector(".hostname");
+  hostname.style.color = '#ffaf00';
+
+  const relativeDir = document.querySelector(".relative-dir");
+  relativeDir.style.color = '#005fff';
+
+  const cmdSeparator = document.querySelector(".cmd-separator");
+  cmdSeparator.style.color = '#c0c0c0';
+}
