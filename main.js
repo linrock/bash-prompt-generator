bb9eb7e1964dc0599aa92cb3915e271a3a3a53e0
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 14:41:58 -0500

    Hardcode 256 colors. Use xterm codes for html colors

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 {
   const username = document.querySelector(".username");
-  username.style.color = '#ffff00';
+  username.style.color = COLORS[226]; // '#ffff00';
 
   const separator = document.querySelector(".separator");
-  separator.style.color = '#ffd700';
+  separator.style.color = COLORS[220]; // '#ffd700';
 
   const hostname = document.querySelector(".hostname");
-  hostname.style.color = '#ffaf00';
+  hostname.style.color = COLORS[214]; // '#ffaf00';
 
   const relativeDir = document.querySelector(".relative-dir");
-  relativeDir.style.color = '#005fff';
+  relativeDir.style.color = COLORS[33]; // '#005fff';
 
   const cmdSeparator = document.querySelector(".cmd-separator");
-  cmdSeparator.style.color = '#c0c0c0';
+  cmdSeparator.style.color = '#fff';
 }
