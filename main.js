e1a5d20e9371bbffe5591eeef1afd69d2a19b8c5
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 14:56:31 -0500

    Enable changing xterm color codes to see prompt and PS1

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 {
+  const colorTheme = [
+    226,  // '#ffff00'
+    220,  // '#ffd700'
+    214,  // '#ffaf00'
+    33,   // '#005fff'
+  ];
+
+  const code0 = colorTheme[0];
   const username = document.querySelector(".username");
-  username.style.color = COLORS[226]; // '#ffff00';
+  username.style.color = COLORS[code0];
+  const usernameColorCode = document.querySelector(".ps1-username-color");
+  usernameColorCode.textContent = code0;
 
+  const code1 = colorTheme[1];
   const separator = document.querySelector(".separator");
-  separator.style.color = COLORS[220]; // '#ffd700';
+  separator.style.color = COLORS[code1];
+  const separatorColorCode = document.querySelector('.ps1-at-color');
+  separatorColorCode.textContent = code1;
 
+  const code2 = colorTheme[2];
   const hostname = document.querySelector(".hostname");
-  hostname.style.color = COLORS[214]; // '#ffaf00';
+  hostname.style.color = COLORS[code2];
+  const hostnameColorCode = document.querySelector('.ps1-hostname-color');
+  hostnameColorCode.textContent = code2;
 
+  const code3 = colorTheme[3];
   const relativeDir = document.querySelector(".relative-dir");
-  relativeDir.style.color = COLORS[33]; // '#005fff';
+  relativeDir.style.color = COLORS[code3];
+  const relativeDirColorCode = document.querySelector('.ps1-relative-dir-color');
+  relativeDirColorCode.textContent =  code3;
 
   const cmdSeparator = document.querySelector(".cmd-separator");
   cmdSeparator.style.color = '#fff';
