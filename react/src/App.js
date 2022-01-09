ca5ba034dc9bad695e410646f4d27d1022cc2b1c
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 18:58:50 -0500

    Get customizing color squares working

--- react/src/App.js
+++ react/src/App.js
@@ -3,43 +3,58 @@ import { useState } from 'react';
 import { COLORS } from './colors';
 import './App.css';
 
-function Squares256() {
-  // build html for the 256 color squares
-  let elements = [];
-
-  // 2 rows of 8 for the first 16 basic colors
-  for (let j = 0; j < 2; j++) {
-    for (let i = 0; i < 8; i++) {
-      const code = j*8 + i;
-      elements.push(<div className="square" style={{ background: COLORS[code] }} data-code={code}></div>);
-    }
-    elements.push(<div className="clear-left"></div>);
-  }
-  // 12 rows of length 12
-  for (let j = 0; j < 18; j++) {
-    for (let i = 0; i < 12; i++) {
-      const code = 16 + j*12 + i;
-      elements.push(<div className="square" style={{ background: COLORS[code] }} data-code={code}></div>);
-    }
-    elements.push(<div className="clear-left"></div>);
-  }
-  for (let j = 0; j < 2; j++) {
-    for (let i = 0; i < 12; i++) {
-      const code = 232 + j*12 + i;
-      elements.push(<div className="square" style={{ background: COLORS[code] }} data-code={code}></div>);
-    }
-    elements.push(<div className="clear-left"></div>);
-  }
-  return <>{elements}</>;
-}
-
-
-function App() {
+export default function() {
   const [selectedColorInd, setSelectedColorInd] = useState(null);
   const [color0, setColor0] = useState(226);
   const [color1, setColor1] = useState(220);
   const [color2, setColor2] = useState(214);
   const [color3, setColor3] = useState(33);
+
+  // color squares that can be clicked to set the color
+  const Square = ({ code }) => (
+    <div className="square"
+      style={{ background: COLORS[code] }}
+      onMouseDown={() => {
+        if (selectedColorInd === null) {
+          return;
+        }
+        if (selectedColorInd === 0) setColor0(code);
+        if (selectedColorInd === 1) setColor1(code);
+        if (selectedColorInd === 2) setColor2(code);
+        if (selectedColorInd === 3) setColor3(code);
+      }}></div>
+  );
+
+  // build html for the 256 color squares
+  const Squares256 = () => {
+    let elements = [];
+
+    // 2 rows of 8 for the first 16 basic colors
+    for (let j = 0; j < 2; j++) {
+      for (let i = 0; i < 8; i++) {
+        const code = j*8 + i;
+        elements.push(<Square code={code} key={code} />);
+      }
+      elements.push(<div className="clear-left" key={`clear-${j}`}></div>);
+    }
+    // 12 rows of length 12
+    for (let j = 0; j < 18; j++) {
+      for (let i = 0; i < 12; i++) {
+        const code = 16 + j*12 + i;
+        elements.push(<Square code={code} key={code} />);
+      }
+      elements.push(<div className="clear-left" key={`clear-${16 + j}`}></div>);
+    }
+    for (let j = 0; j < 2; j++) {
+      for (let i = 0; i < 12; i++) {
+        const code = 232 + j*12 + i;
+        elements.push(<Square code={code} key={code} />);
+      }
+      elements.push(<div className="clear-left" key={`clear-${232 + j}`}></div>);
+    }
+    return <>{elements}</>;
+  }
+
   return (
     <>
       <section>
@@ -61,19 +76,39 @@ function App() {
         <div className="color-wrapper">
           <div className="color-theme">
             <div className="color-choice">
-              <div className="color-preview" id="color0-preview"></div>
+              <div className="color-preview"
+                   style={{ background: COLORS[color0] }}
+                   onClick={() => {
+                     console.log('set selected color index 0');
+                     setSelectedColorInd(0);
+                   }}></div>
               <input type="number" min="0" max="255" id="color0" defaultValue={color0} />
             </div>
             <div className="color-choice">
-              <div className="color-preview" id="color1-preview"></div>
+              <div className="color-preview"
+                   style={{ background: COLORS[color1] }}
+                   onClick={() => {
+                     console.log('set selected color index 1');
+                     setSelectedColorInd(1);
+                   }}></div>
               <input type="number" min="0" max="255" id="color1" defaultValue={color1} />
             </div>
             <div className="color-choice">
-              <div className="color-preview" id="color2-preview"></div>
+              <div className="color-preview"
+                   style={{ background: COLORS[color2] }}
+                   onClick={() => {
+                     console.log('set selected color index 2');
+                     setSelectedColorInd(2);
+                   }}></div>
               <input type="number" min="0" max="255" id="color2" defaultValue={color2} />
             </div>
             <div className="color-choice">
-              <div className="color-preview" id="color3-preview"></div>
+              <div className="color-preview"
+                   style={{ background: COLORS[color3] }}
+                   onClick={() => {
+                     console.log('set selected color index 3');
+                     setSelectedColorInd(3);
+                   }}></div>
               <input type="number" min="0" max="255" id="color3" defaultValue={color3} />
             </div>
           </div>
@@ -92,5 +127,3 @@ function App() {
     </>
   );
 }
-
-export default App;
