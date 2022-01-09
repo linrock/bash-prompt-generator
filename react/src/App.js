94f09497b45d512fafa1bb849f407487cc568602
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 17:43:19 -0500

    Get a basic react app working

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bash prompt generator</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
