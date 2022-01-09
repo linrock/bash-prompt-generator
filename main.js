b1d60ebb008cd349555ab5d6a43f30ac8dc39ae3
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 15:55:51 -0500

    Visible color previews

--- main.js
+++ main.js
@@ -31,24 +31,32 @@
     username.style.color = COLORS[code0];
     const usernameColorCode = document.querySelector(".ps1-username-color");
     usernameColorCode.textContent = code0;
+    const usernameColorCodePreview = document.querySelector('#color0-preview');
+    usernameColorCodePreview.style.background = COLORS[code0];
 
     const code1 = colorTheme[1];
     const separator = document.querySelector(".separator");
     separator.style.color = COLORS[code1];
     const separatorColorCode = document.querySelector('.ps1-at-color');
     separatorColorCode.textContent = code1;
+    const separatorColorCodePreview = document.querySelector('#color1-preview');
+    separatorColorCodePreview.style.background = COLORS[code1];
 
     const code2 = colorTheme[2];
     const hostname = document.querySelector(".hostname");
     hostname.style.color = COLORS[code2];
     const hostnameColorCode = document.querySelector('.ps1-hostname-color');
     hostnameColorCode.textContent = code2;
+    const hostnameColorCodePreview = document.querySelector('#color2-preview');
+    hostnameColorCodePreview.style.background = COLORS[code2];
 
     const code3 = colorTheme[3];
     const relativeDir = document.querySelector(".relative-dir");
     relativeDir.style.color = COLORS[code3];
     const relativeDirColorCode = document.querySelector('.ps1-relative-dir-color');
     relativeDirColorCode.textContent =  code3;
+    const relativeDirColorCodePreview = document.querySelector('#color3-preview');
+    relativeDirColorCodePreview.style.background = COLORS[code3];
 
     const cmdSeparator = document.querySelector(".cmd-separator");
     cmdSeparator.style.color = '#fff';
